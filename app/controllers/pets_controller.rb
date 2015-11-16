d4ea61bc4a5aafc793bf79b3f3be83223a4ab95d
Thom Householder <thomhouseholder@gmail.com>
2015-11-16 16:23:14 -0500

    added some pet CRUDS - create

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,31 @@
 class PetsController < ApplicationController
 
   def index
+    @pets = Pet.all
   end
-  
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.create!(pet_params)
+
+    if @pet.save
+      redirect_to @pet
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @pet = Pet.find(params[:id])
+  end
+
+
+private
+  def pet_params
+    params.require(:pet).permit(:name, :breed, :gender, :dob)
+  end
+
 end
