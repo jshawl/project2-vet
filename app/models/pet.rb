6800a741ffab51ca353a1522b8086eb839b05741
Thom Householder <thomhouseholder@gmail.com>
2015-11-16 12:16:44 -0500

    created pets table

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,4 @@
 class Pet < ActiveRecord::Base
   has_many :appointments
-  has_many :users, through :appointments
+  has_many :users, through: :appointments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116154503) do
+ActiveRecord::Schema.define(version: 20151116171444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+    t.string   "name"
+    t.string   "breed"
+    t.string   "gender"
+    t.string   "dob"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
