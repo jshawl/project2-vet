26c0945385e0e7d51baa624fe606c17b30aa3aa4
Thom Householder <thomhouseholder@gmail.com>
2015-11-16 12:13:51 -0500

    added and set up devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :appointments
-  has_many :pets, through :appointments
+  has_many :pets, through: :appointments
 end
