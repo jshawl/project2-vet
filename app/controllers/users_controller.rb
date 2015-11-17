6356dddb445cd84b25a2e4d32d25e8c59fa27183
Thom Householder <thomhouseholder@gmail.com>
2015-11-17 10:53:56 -0500

    created user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,25 +5,45 @@ class UsersController < ApplicationController
   end
 
   def show
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def update
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create!(user_params)
+
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:notice] = "#{@user.name} was successfully updated."
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    @user = User.all
+    redirect_to users_path
   end
 
   private
   def user_params
+    params.require(:user).permit(:name, :email)
   end
 
 
